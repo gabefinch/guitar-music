22b3eff82c6d0abed1f3a17cba0672724e598a22
Gabe Finch <gabefichg@gmail.com>
2021-04-21 21:40:15 -0700

    Get Tailwind working with Craco

--- src/App.js
+++ src/App.js
@@ -1,6 +1,51 @@
 function App() {
   return (
-    <div className="App">
+    <div className="flex items-center justify-center">
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
+        <div className="relative bgWhite py-6 px-6 rounded-3xl w-64 my-4 shadow-xl">
+          <p className="text-xl font-semibold my-2">Key Root: E</p>
+          <div className="flex space-x-2 text-gray-400 text-sm my-3">
+            <p>Mode: Major Diatonic</p>
+          </div>
+          <div className="border-t-2" />
+          <div className="flex justify-between">
+            <div className="my-2">
+              <p className="font-semibold text-base mb-2">Scale</p>
+              <div className="flex space-x-2">E (I)</div>
+              <div className="flex space-x-2">F# (II)</div>
+              <div className="flex space-x-2">G# (III)</div>
+              <div className="flex space-x-2">A (IV)</div>
+              <div className="flex space-x-2">B (V)</div>
+              <div className="flex space-x-2">C# (VI)</div>
+              <div className="flex space-x-2">D# (VII)</div>
+            </div>
+            <div className="my-2">
+              <p className="font-semibold text-base mb-2">Relation</p>
+              <p className="text-base text-gray-400 font-semibold">Tonic</p>
+              <p className="text-base text-gray-400 font-semibold">
+                Supertonic
+              </p>
+              <p className="text-base text-gray-400 font-semibold">Mediant</p>
+              <p className="text-base text-gray-400 font-semibold">
+                Subdominant
+              </p>
+              <p className="text-base text-gray-400 font-semibold">Dominant</p>
+              <p className="text-base text-gray-400 font-semibold">
+                Submediant
+              </p>
+              <p className="text-base text-gray-400 font-semibold">Subtonic</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+{
+  /* <div classNameName="App">
       Key Root: E
       Mode: Diatonic Major
 
@@ -22,8 +67,5 @@ function App() {
       C# min (vi)
       D# dim (viiº)
 
-    </div>
-  );
+    </div> */
 }
-
-export default App;
